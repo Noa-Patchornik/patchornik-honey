ff997dae99c802f79091e635392ed2598ada2396
Noa-Patchornik <noap2580@gmail.com>
2026-09-13 18:22:37 +0300

    fix the app file to use the links to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/scrollToTop';
+
 import Home from './pages/Home';
+import AboutPage from './pages/aboutPage';
+import ProductsPage from './pages/productsPage';
+import KosherPage from './pages/KosherPage';
+import PressPage from './pages/pressPage';
+import HoneyGalleryPage from './pages/honeyGalleryPage';
+import FieldGalleryPage from './pages/fieldGalleryPage';
+import ContactPage from './pages/contactPage';
 
 function App() {
   return (
@@ -8,6 +16,13 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/kosher" element={<KosherPage />} />
+        <Route path="/press" element={<PressPage />} />
+        <Route path="/honey-gallery" element={<HoneyGalleryPage />} />
+        <Route path="/field-gallery" element={<FieldGalleryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
